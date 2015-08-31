bf5a1ce0c74cc780317bf23bc3b2159b34165394
Stella Zhao <tmrvivian@gmail.com>
2015-08-30 22:20:50 -0400

    location service works for real. last time was because gulp did not update in time

--- browser/js/newEvent/newEvent.ctrl.js
+++ browser/js/newEvent/newEvent.ctrl.js
@@ -2,9 +2,9 @@ app.controller('locSearch',function($scope){
 	$scope.location = '';
     $scope.doSearch = function(){
         if($scope.location === ''){
-            alert('Directive did not update the location property in parent controller.');
+            alert('You did not enter a location.');
         } else {
-            alert('Yay. Location: ' + $scope.location);
+            alert($scope.location);
         }
     };
 });
